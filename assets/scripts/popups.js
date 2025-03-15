4ed15145c040b398489e958d47e6c0cf57fbd14f
Godzilaer <114835170+Godzilaer@users.noreply.github.com>
2025-03-15 15:41:05 -0400

    Update popups.js

--- assets/scripts/popups.js
+++ assets/scripts/popups.js
@@ -24,7 +24,6 @@ function getRandomMessageBig() {
     const randomIndex = Math.floor(Math.random() * messages.length);
     return messages[randomIndex];
 }
-document.getElementById('randomMessage').innerText = getRandomMessageBig();
 
 setInterval(function() {
     $('#myModal').modal('show');
