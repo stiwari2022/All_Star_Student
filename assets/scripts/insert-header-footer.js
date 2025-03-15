5d984238190d8b55b33a25a3a8a188373a348a4f
Godzilaer <114835170+Godzilaer@users.noreply.github.com>
2025-03-15 11:11:21 -0400

    basic

---
+++ assets/scripts/insert-header-footer.js
@@ -0,0 +1,47 @@
+fetch('/assets/header-footer/header.html')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response not ok: ' + response.statusText);
+        }
+
+        return response.text();
+    })
+    .then(data => {
+        document.body.insertAdjacentHTML("afterbegin", data);
+    })
+    .catch(error => {
+        console.error('Header fetch error:', error);
+    });
+
+fetch('/assets/header-footer/footer.html')
+.then(response => {
+    if (!response.ok) {
+        throw new Error('Network response not ok: ' + response.statusText);
+    }
+
+    return response.text();
+})
+.then(data => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(data, 'text/html');
+/*
+    const copyrightText = doc.querySelector("#copyright");
+
+    var originalYear = 2024;
+    var currentYear = new Date().getFullYear();
+    var year = currentYear == originalYear ? originalYear : originalYear + "-" + currentYear;
+
+
+    copyrightText.innerHTML = `&copy; ${year} Ethan Filip. All rights reserved.`;
+    */
+    
+    //Insert before the first script tag in the body
+    //document.querySelector("body script").insertAdjacentHTML("beforebegin", doc.body.innerHTML);
+    document.body.insertAdjacentHTML("beforeend", doc.body.innerHTML);
+
+    const footerLoadedEvent = new Event('footerLoaded');
+    document.dispatchEvent(footerLoadedEvent);
+})
+.catch(error => {
+    console.error('Footer fetch error:', error);
+});
